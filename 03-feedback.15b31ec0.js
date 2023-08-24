325ca75b3d7e3efafa83e181058ea9050129d33e
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-08-24 14:20:54 +0000

    Deploying to gh-pages from @ NataliaMahera/goit-js-hw-08@b9fbc004580723c5815d3329182df1f887c3804e 🚀

--- 03-feedback.15b31ec0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");document.querySelector(".feedback-form input"),document.querySelector(".feedback-form textarea");e.addEventListener("input",(function(e){const t={email:email,message:message};console.log(email),localStorage.setItem("feedback-form-state",JSON.stringify(t))}));
-//# sourceMappingURL=03-feedback.15b31ec0.js.map
